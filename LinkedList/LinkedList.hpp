6dd1dfd876fd556c8c1d571fdb6269ee962deb5d
Vlad-Vershinin <vladversinin676@gmail.com>
2025-09-24 14:35:13 +0500

    add linked list

---
+++ LinkedList/LinkedList.hpp
@@ -0,0 +1,117 @@
+#ifndef _LINKEDLIST_HPP_
+#define _LINKEDLIST_HPP_
+
+namespace LinkedList {
+
+	template <typename T>
+	struct Node
+	{
+		T data;
+		Node<T>* next;
+	};
+
+	template <typename T>
+	class LinkedList {
+	public:
+		LinkedList() {
+			head = nullptr;
+			count = 0;
+		}
+
+		bool Empty() {
+			return head == nullptr;
+		}
+
+		void PushBack(T d) {
+			Node<T>* new_node = new Node<T>;
+			new_node->data = d;
+			new_node->next = nullptr;
+
+			if (head == nullptr) {
+				head = new_node;
+			}
+			else {
+				Node<T>* current = head;
+
+				while (current->next != nullptr)
+				{
+					current = current->next;
+				}
+				current->next = new_node;
+			}
+			count++;
+		}
+
+		void Insert(T d, int index) {
+			if (index < 0) {
+				throw std::invalid_argument("Index cannot be negative");
+			}
+
+			Node<T>* new_node = new Node<T>;
+			new_node->data = d;
+			new_node->next = nullptr;
+
+			if (index == 0) {
+				new_node->next = head;
+				head = new_node;
+				return;
+			}
+
+			Node<T>* current = head;
+			int counter = 0;
+
+			while (current != nullptr && counter < index - 1) {
+				current = current->next;
+				counter++;
+			}
+
+			if (current == nullptr) {
+				delete new_node;
+				throw std::out_of_range("Index out of range");
+			}
+
+			new_node->next = current->next;
+			current->next = new_node;
+			count++;
+		}
+
+		void Remove(int index) {
+			if (index < 0) {
+				throw std::invalid_argument("Index cannot be negative");
+			}
+
+			if (index == 0) {
+				Node<T>* temp = head;
+				head = head->next;
+				delete temp;
+				return;
+			}
+
+			Node<T>* current = head;
+			int counter = 0;
+
+			while (current != nullptr && counter < index - 1) {
+				current = current->next;
+				counter++;
+			}
+
+			if (current == nullptr || current->next == nullptr) {
+				throw std::out_of_range("Index out of range");
+			}
+
+			Node<T>* temp = current->next;
+			current->next = temp->next;
+			delete temp;
+			count--;
+		}
+
+		int Count() {
+			return count;
+		}
+	private:
+		Node<T>* head;
+		int count;
+	};
+}
+
+#endif // !_LINKEDLIST_HPP_
